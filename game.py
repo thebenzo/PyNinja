a8d78ee25ef90752201490095d9805c58a5bd650
Aman <amanvrma5799@gmail.com>
2024-02-21 23:46:21 +0530

    Create a new surface to render graphics over the existing window

--- game.py
+++ game.py
@@ -13,6 +13,9 @@ class Game:
         self.window = pygame.display.set_mode((1280, 720))
         pygame.display.set_caption('PyNinja')
 
+        # All rendering is done on this surface. It is later scaled to window size
+        self.viewport = pygame.Surface((640, 360))
+
         # Dictionary of game assets with entityType as the key
         self.assets = {
             'player': load_sprite('entities/player.png')
@@ -27,12 +30,12 @@ class Game:
 
     def run(self):
         while True:
-            self.window.fill((78, 159, 229))
+            self.viewport.fill((78, 159, 229))
 
             ''' This takes benefit of the fact that booleans convert to integer value implicitly 
                 when used with arithmetic operators. '''
             self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.window)
+            self.player.render(self.viewport)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -48,6 +51,9 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_d:
                         self.movement[1] = False
+
+            # Scaling viewport to match window size which gives a zoomed-in view to the game world
+            self.window.blit(pygame.transform.scale(self.viewport, self.window.get_size()), (0, 0))
 
             pygame.display.update()
             # Limit the game to 60 FPS
