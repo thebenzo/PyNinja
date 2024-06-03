eea1b04d5c50557f570f9c966088aa2070f116ae
Aman <amanvrma5799@gmail.com>
2024-06-03 17:15:10 +0530

    Implement basic movement

--- PyNinja.py
+++ PyNinja.py
@@ -13,12 +13,32 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        self.pos = [100, 200]
+
+        # Movement state on x-axis
+        self.movement_x = [False, False]
+
     def run(self):
         """ Main game loop """
         while True:
             self.window.fill((30, 150, 220))
 
+            pygame.draw.rect(self.window, (255, 255, 255), (self.pos[0], self.pos[1], 20, 20))
+
+            # Booleans implicitly converts to integers when arithmetic operation are performed on them
+            self.pos[0] += self.movement_x[1] - self.movement_x[0]
+
             for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        self.movement_x[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement_x[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        self.movement_x[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement_x[1] = False
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
